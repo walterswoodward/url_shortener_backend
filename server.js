eeeb467c8cc79d0e3522ebf256e4255f7eacff06
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-23 19:46:32 -0500

    add missing catch

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.get("/new/:urlToShorten(*)", (req, res) => {
       }
     )
     data.save()
-    .then(data => res.status(201).json({ data }))
+    .then(data => res.status(201).json(data))
     .catch(error => res.status(500).json({ error }));
   } else {
     let data = new shortUrl(
